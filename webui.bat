27c72a0d2be275fd429b2865135e3877c9c0642f
Deni Rohimat <rohimatdeni@gmail.com>
2026-02-15 13:17:14 +0700

    Fixes webui launch on specific environments
    Updates the webui launch script to ensure proper execution
    on environments with specific Python configurations.
    
    This resolves potential issues related to module paths and
    allows the web UI to start correctly. The updated script
    explicitly sets the PYTHONPATH and uses the full path to the
    Python executable.

--- webui.bat
+++ webui.bat
@@ -3,5 +3,5 @@ set CURRENT_DIR=%CD%
 echo ***** Current directory: %CURRENT_DIR% *****
 set PYTHONPATH=%CURRENT_DIR%
 
-rem set HF_ENDPOINT=https://hf-mirror.com
-streamlit run .\webui\Main.py --browser.gatherUsageStats=False --server.enableCORS=True
+"C:\Users\deroh\AppData\Local\Python\pythoncore-3.14-64\python.exe" -m streamlit run .\webui\Main.py --browser.gatherUsageStats=False --server.enableCORS=True --server.port 8502 --server.address 127.0.0.1
+pause
